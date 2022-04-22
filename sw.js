eafe7d5889d52b5cc15d0ef2e2194e258e04688f
aad1416 <aad1416@ya.ru>
2022-04-22 17:33:18 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ const addResourcesToCache = async (resources) => {
 self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
+	  "/",
       "/index.html",
 	  "/m.json",
 	  "/aad1416.svg",
